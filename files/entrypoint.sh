65d4c44c4ee7f700cca83c0a56a098440a2b9199
vistal8 <54735148+vistal8@users.noreply.github.com>
2024-08-06 10:46:03 +0800

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'e599fae9-d91b-4aa2-a4e4-b64635ea10d0'}
 WEB_USERNAME=${WEB_USERNAME:-'admin'}
 WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 
